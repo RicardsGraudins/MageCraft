e6feff19f89cba44257f1f34dd853d43119df52e
Ricards <G00314715@gmit.ie>
2017-10-08 14:20:31 +0100

    3. WTForms Integration

--- runme.py
+++ runme.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request, session, redirect, flash, url_for
 from flask_pymongo import PyMongo
 import bcrypt
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
+from wtforms.validators import InputRequired, Length, Email
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecret'
@@ -13,21 +16,30 @@ mongo = PyMongo(app)
 def index():
 	return render_template('game.html')
 	
+class RegisterForm(FlaskForm):
+	username = StringField('Username', validators=[InputRequired('Username is required!'), Length(min=4, max=12, message='Must be between 4 and 12 characters.')])
+	password = PasswordField('Password', validators=[InputRequired('Password is required!'), Length(min=4, max=12, message='Must be between 4 and 12 characters.')])
+	email = StringField('Email', validators=[InputRequired('Email address is required!'), Email('A valid email is required!')])
+
+	
 @app.route('/register', methods=['POST', 'GET'])
 def register():
-    if request.method == 'POST':
-        users = mongo.db.users
-        existing_user = users.find_one({'name' : request.form['username']})
+	form = RegisterForm()
 	
-        if existing_user is None:
-            hashpass = bcrypt.hashpw(request.form['pass'].encode('utf-8'), bcrypt.gensalt())
-            users.insert({'name' : request.form['username'], 'email' : request.form['email'], 'password' : hashpass})
-            session['username'] = request.form['username']
-            return redirect(url_for('profile'))
-        
-        return 'That username already exists!'
-
-    return render_template('register.html')
+	#POST
+	if form.validate_on_submit():
+		users = mongo.db.users
+		existing_user = users.find_one({'name' : request.form['username']})
+		
+		if existing_user is None:
+			hashpass = bcrypt.hashpw(request.form['password'].encode('utf-8'), bcrypt.gensalt())
+			users.insert({'name' : request.form['username'], 'email' : request.form['email'], 'password' : hashpass})
+			session['username'] = request.form['username']
+			return redirect(url_for('profile'))
+			
+		flash('That username already exists!')
+		
+	return render_template('register.html', form=form)
 	
 @app.route('/login', methods=['POST'])
 def login():
